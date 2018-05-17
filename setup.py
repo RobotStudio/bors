e6ccba324722cb60f226c037fd82e072234e9bcf
Bobby <karma0@gmail.com>
2018-05-17 13:18:36 -0600

    Updated setup.py with package info

--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
-from setuptools import setup, find_packages
-import sys, os
+"""Setup script to package project"""
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
+import os
+from setuptools import setup
 
 
-version = '0.0.1'
+HERE = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(HERE, 'README.rst')).read()
+
+VERSION = '0.0.1'
 
 
 setup(
     name='bors',
-    packages=['bors', 'bors.envs', 'bors.envs.stocks'],
-    version=version,
-    description="A highly flexible and extensible service integration framework for scraping the web or consuming APIs.",
+    packages=[
+        'bors',
+        'bors.api',
+        'bors.app',
+        'bors.common',
+        'bors.generics',
+        'bors.strategies',
+        'bors.algorithms',
+    ],
+    version=VERSION,
+    description="A highly flexible and extensible service integration "
+    "framework for scraping the web or consuming APIs.",
     long_description=README,
     classifiers=[],
-    keywords='web-scraper api-integrator scraping scraper data-integration data-ingestion',
+    keywords="web-scraper api-integrator scraping scraper data-integration"
+    "data-ingestion",
     author='Bobby',
     author_email='karma0@gmail.com',
     url='https://github.com/RobotStudio/bors',
