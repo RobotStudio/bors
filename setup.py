f9036ef60964397da7da6bec03ef9d337e2bd446
Bobby <karmma0@gmail.com>
2018-05-18 12:08:09 -0600

    Changed README to MD

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(HERE, 'README.rst')).read()
+README = open(os.path.join(HERE, 'README.md')).read()
 
 VERSION = '0.0.1'
 
