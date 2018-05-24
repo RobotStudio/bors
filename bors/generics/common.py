63a1265fd57866037b6c3b9a52dc5566e10e06a7
Bobby <karmma0@gmail.com>
2018-05-23 23:20:39 -0600

    Added some basic primitives for API integrations

---
+++ bors/generics/common.py
@@ -0,0 +1,39 @@
+"""Generic exchange schemas"""
+
+from marshmallow import Schema, post_load
+from marshmallow import fields as f
+
+from bors.common.dotobj import DotObj
+
+
+class GenericObject(DotObj):
+    """Generic object with dot or hash style get/set"""
+    pass
+
+
+class GenericSchema(Schema):
+    """Schema from which all other schemas should inherit"""
+    @post_load
+    def make_object(self, data):  # pylint: disable=no-self-use
+        """Generate an object for passing to the exchange API"""
+        return GenericObject(data)
+
+
+class ApiFacadeSchema(GenericSchema):
+    """Used to define an API integration (facade)"""
+    name = f.Str(required=True)
+    call = f.Method(required=True)
+
+
+class RequestSchema(GenericSchema):
+    """Generic API result (inherit to use a schema for result creation)"""
+    class Meta:
+        """All results should be strict"""
+        strict = True
+
+
+class ResultSchema(GenericSchema):
+    """Generic API result (inherit to use a schema for result creation)"""
+    class Meta:
+        """All results should be strict"""
+        strict = True
