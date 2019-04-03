439827032ca40693efc719591da8d98f6f79fb87
Bobby <karma0@gmail.com>
2019-04-03 14:17:29 -0600

    Simplify logging for a bit

--- bors/app/log.py
+++ bors/app/log.py
@@ -1,20 +1,5 @@
 """Log Util"""
 
 import logging
-
-
-class LoggerMixin:
-    """Mixin to be used to simplify the logging interface"""
-    def create_logger(self):
-        """Generates a logger instance from the singleton"""
-        name = "bors"
-        if hasattr(self, "name"):
-            name = self.name
-
-        self.log = logging.getLogger(name)
-        try:
-            lvl = self.conf.get_log_level()
-        except AttributeError:
-            lvl = self.context.get("log_level", None)
-
-        self.log.setLevel(getattr(logging, lvl, logging.INFO))
+logger = logging.getLogger('bors')
+logger.setLevel(logging.INFO)
